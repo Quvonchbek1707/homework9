d1516e7190e775ec28cb462fdf9cc13a55ded42c
Quvonchbek1707 <106889398+Quvonchbek1707@users.noreply.github.com>
2025-08-22 21:00:40 +0500

    return to previous version task2.js

--- task2.js
+++ task2.js
@@ -1,4 +1,28 @@
-let arr = JSON.parse(prompt("Array bering:"));
+let str = prompt("Array bering: ")
+let arr = str
+    .replace("[", "")
+    .replace("]", "")
+    .split(",")
+    .map(num => Number(num.trim()));
+
+// I don't know in what format the input array is going to be given. So I assume [ ] will be included in both ends.
+// Previous version contained long procedure to get array in both formats.
+
+// let arr = JSON.parse(prompt("Array bering:"));
+
+function formatPhoneNumber(arr) {
+    if (arr.length !== 10 || arr.some(isNaN)) {
+        console.log("Error: enter exactly 10 digits");
+        return;
+    }
+    let area = arr.slice(0, 3).join("");
+    let middle = arr.slice(3, 6).join("");
+    let last = arr.slice(6).join("");
+    let ans = `(${area}) ${middle}-${last}`;
+    console.log(ans);
+}
+
+formatPhoneNumber(arr);
 
 function sumOfCubes(arr){
     let total = 0;
