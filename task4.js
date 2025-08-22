9e7cfc37141986386539b55230bf47da302164d3
Quvonchbek1707 <106889398+Quvonchbek1707@users.noreply.github.com>
2025-08-22 21:01:38 +0500

    Update task4.js

--- task4.js
+++ task4.js
@@ -1,5 +1,14 @@
-let arr = JSON.parse(prompt("Array bering:"));
+let str = prompt("Array bering: ")
+let arr = str
+    .replace("[", "")
+    .replace("]", "")
+    .split(",")
+    .map(num => Number(num.trim()));
 
+// I don't know in what format the input array is going to be given. So I assume [ ] will be included in both ends.
+// Previous version contained long procedure to get array in both formats.
+
+// let arr = JSON.parse(prompt("Array bering:"));
 function findPrimeNumber(arr){
     for(let i =0; i<arr.length; i++){
         let val = arr[i]
@@ -15,3 +24,4 @@ function findPrimeNumber(arr){
 }
 
 findPrimeNumber(arr)
+
