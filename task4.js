1a8095518f0b12b684920e93a2a4c42e08775f59
Quvonchbek1707 <106889398+Quvonchbek1707@users.noreply.github.com>
2025-08-22 20:13:37 +0500

    Update task4.js

--- task4.js
+++ task4.js
@@ -1,9 +1,5 @@
-let str = prompt("Array bering: ")
-let arr = str
-    .replace("[", "")
-    .replace("]", "")
-    .split(",")
-    .map(num => Number(num.trim()));
+let arr = JSON.parse(prompt("Array bering:"));
+
 function findPrimeNumber(arr){
     for(let i =0; i<arr.length; i++){
         let val = arr[i]
